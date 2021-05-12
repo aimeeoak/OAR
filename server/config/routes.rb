1d3089cc679016ebdb4b0f1c34b76cb8f3f5e594
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-12 14:52:19 -0700

    destroyed searches. gone:

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
     resources :users
   end
   namespace :api do
-    resources :searches
-  end
+    end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
